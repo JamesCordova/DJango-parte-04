360ed8cf158f40227c3b599d55d6d4771bb38c8d
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-03 10:08:04 -0500

    la tabla ahora aparece en el localhost admin

--- src/lugares/admin.py
+++ src/lugares/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Lugar
+admin.site.register(Lugar)
--- src/lugares/models.py
+++ src/lugares/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Lugar(model.Model):
+class Lugar(models.Model):
     nombre = models.TextField(max_length=30,blank=True)
     nroTuristasYear = models.IntegerField()
     pais = models.CharField(max_length=20,primary_key=True)
