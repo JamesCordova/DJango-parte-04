53d25ff284ff69f878343a299dd8d809b64f0dda
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-09 09:05:13 -0500

    Se agregaron nuevos campos, en los que se define un nuevo campo con un default en este caso True

--- src/personas/models.py
+++ src/personas/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-    nombres = models.TextField()
-    apellidos = models.TextField()
+    nombres = models.TextField(max_length=30,blank=False) #ahora un campo es obligatorio
+    apellidos = models.TextField(max_length=30,blank=False)
     edad = models.TextField()
+    donador = models.BooleanField()
