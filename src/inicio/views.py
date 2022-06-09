30d07fdec3533bc3c5b7ce0d6a46dfd7c5522caa
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-09 09:20:36 -0500

    pagina de inicio fue arreglada porque HTTPResponse y se manda a consola algunos parametros

--- src/inicio/views.py
+++ src/inicio/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def myHomeView(*args,**kwargs):
-    return HTTPResponse('<h1>Hola mundo desde Django</h1>')
+def myHomeView(request,*args,**kwargs):
+    print(args,kwargs)
+    print(request.user)
+    return HttpResponse('<h1>Hola mundo desde Django</h1>')
 
 def myOtraVista(request, *args, **kwargs):
     return HttpResponse('<h1>Solo otra pagina</h1>')
