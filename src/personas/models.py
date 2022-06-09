11ea36673d54c90ebc7516553be5fd36db93ac96
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-09 09:07:10 -0500

    algunos campos tienen nuevos argumentos, para hacer que sean obligatorios

--- src/personas/models.py
+++ src/personas/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Persona(models.Model):
     nombres = models.TextField(max_length=30,blank=False) #ahora un campo es obligatorio
     apellidos = models.TextField(max_length=30,blank=False)
-    edad = models.TextField()
+    edad = models.TextField(blank=False)
     donador = models.BooleanField()
