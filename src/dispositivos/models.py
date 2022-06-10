204e7a50a36f0feefc6daef4eeee2fe91e5a5694
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-10 07:38:02 -0500

    se hizo uso de opciones como dafualt y blank de nuevo, solo los valores null afectan a la DataBase, porque esta puramente relacionado a su almacenamiento, blank lo esta con la validacion

--- src/dispositivos/models.py
+++ src/dispositivos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Dispositivo(models.Model):
-    nombre = models.TextField(max_length=10)
-    marca = models.TextField(max_length=10)
+    nombre = models.TextField(max_length=10,blank=False)
+    marca = models.TextField(default="unknown",max_length=10)
     numero = models.IntegerField(blank=False)
     funcional = models.BooleanField()
