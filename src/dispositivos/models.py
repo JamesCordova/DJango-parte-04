19652a6827c7606b266bd8306dc4c3928bc21b94
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-10 07:24:51 -0500

    se agrego un nuveo campo el cual como otros valores no lo tenian, se definio un valor default desde consola

--- src/dispositivos/models.py
+++ src/dispositivos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Dispositivo(models.Model):
-    nombre = models.TextField()
-    marca = models.TextField()
-    numero = models.IntegerField()
-    
+    nombre = models.TextField(max_length=10)
+    marca = models.TextField(max_length=10)
+    numero = models.IntegerField(blank=False)
+    funcional = models.BooleanField()
