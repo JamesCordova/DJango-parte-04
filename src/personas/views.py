53e936c30678a0c98f6e6641d7c0a04bed95c283
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-16 07:59:31 -0500

    la app personas han sido independizadas, utilizando templates dentro de la app

--- src/personas/views.py
+++ src/personas/views.py
@@ -12,4 +12,4 @@ def personaTestView(request):
     context = {
         'objeto': obj,
     }
-    return render(request, 'personas/test.html', context)
+    return render(request, 'personas/descripcion.html', context)
