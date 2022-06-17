130ee75f1b9ea6203c6f83e9b875b6a1e5553e17
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-17 10:04:37 -0500

    si queremos no mostrar un field, podemos definir un default para ese field en los models de la app

--- src/personas/models.py
+++ src/personas/models.py
@@ -5,4 +5,4 @@ class Persona(models.Model):
     nombres = models.CharField(max_length=30,blank=False) #ahora un campo es obligatorio
     apellidos = models.CharField(max_length=30,blank=False)
     edad = models.IntegerField(blank=False)
-    donador = models.BooleanField()
+    donador = models.BooleanField(default=False)
