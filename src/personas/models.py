b696d35fcd1c3b5c6c5c90eaecd6229a0faffa65
JamesCordova <jcordovas@unsa.edu.pe>
2022-06-25 19:21:17 -0500

    ahora la url sera dinamica independientemente de que ruta tenga, se utilizara el nombre del path que queremos llegar

--- src/personas/models.py
+++ src/personas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Persona(models.Model):
@@ -8,5 +9,6 @@ class Persona(models.Model):
     donador = models.BooleanField(default=False)
 
     def get_absolute_url(self):
-        return '/personas/' + str(self.id) + '/'
+        #return '/personas/' + str(self.id) + '/' # no es dinamico asi que para hacerlo de esa manera usaremos reverse
+        return reverse('browsing', kwargs = {'myID': self.id})
     
